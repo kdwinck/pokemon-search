ddf70ae17a4a175e943871ce399942677ecd8ad3
Kyle Winckler <kyle.winckler@gmail.com>
2016-10-06 22:57:19 -0700

    completed user input by ID and basic api call

--- scripts/models/searchModel.js
+++ scripts/models/searchModel.js
@@ -2,8 +2,17 @@
 
   var searchModel = {};
 
-  searchModel.getDataById = function() {
-    $.get('http://pokeapi.co/api/v2/pokemon/1/')
+  searchModel.getUserInput = function(callback) {
+    $('#submit').on('click', function(event) {
+      event.preventDefault();
+      var userInput = $('#id').val();
+      console.log(userInput);
+      callback(userInput);
+    });
+  },
+
+  searchModel.getDataById = function(userInput) {
+    $.get('http://pokeapi.co/api/v2/pokemon/' + userInput + '/')
     .done(function(data) {
       console.log(data.name);
     });
